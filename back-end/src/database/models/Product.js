1fd6308424da03f0cd63dc5a5ed8614ada96218c
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-27 17:18:42 -0300

    feat: products model

---
+++ back-end/src/database/models/Product.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL(4, 2),
+    url_image: DataTypes.STRING
+  },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'products',
+    });
+
+  return Product;
+};
