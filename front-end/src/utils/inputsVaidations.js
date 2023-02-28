b0f31d8a8a1850613846cad322221140857de35c
Victor Matias <fvictor1705@gmail.com>
2023-02-28 18:30:56 -0300

    feat: create input validations

---
+++ front-end/src/utils/inputsVaidations.js
@@ -0,0 +1,22 @@
+const validateNameInput = (name, setter) => {
+  const DOZE = 12;
+  const isValid = name.length > DOZE;
+  setter((prevState) => (
+    { ...prevState, name: { ...prevState.name, isValid } }));
+};
+
+const validatePassInput = (pass, setter) => {
+  const SEIS = 6;
+  const isValid = pass.length > SEIS;
+  setter((prevState) => (
+    { ...prevState, pass: { ...prevState.pass, isValid } }));
+};
+
+const validateEmailInput = (email, setter) => {
+  const emailRegex = /\S+@\S+\.\S+/;
+  const isValid = emailRegex.test(email);
+  setter((prevState) => (
+    { ...prevState, email: { ...prevState.email, isValid } }));
+};
+
+export { validateNameInput, validateEmailInput, validatePassInput };
