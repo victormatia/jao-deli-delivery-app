cf8ee0770f6979d98803c6e617152e4c6af761a3
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-28 17:03:35 -0300

    refactor: token validation in login route

--- back-end/src/routes/login.route.js
+++ back-end/src/routes/login.route.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const { tokenAuthorization } = require('../middleware/JwtAuthentication');
 
 const loginController = require('../controller/login.controller');
 const { verifyPassword, verifyEmail } = require('../middleware/user.validation');
 
 const route = Router();
 
-route.post('/', verifyEmail, verifyPassword, loginController.login);
+route.post('/', tokenAuthorization, verifyEmail, verifyPassword, loginController.login);
 
 module.exports = route;
