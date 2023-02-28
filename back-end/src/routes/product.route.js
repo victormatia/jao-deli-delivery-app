bc5988a6b0ea3e744a059708eb1feeb9d976a6c6
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-28 17:22:00 -0300

    fix: lint

--- back-end/src/routes/product.route.js
+++ back-end/src/routes/product.route.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const { tokenAuthorization } = require('../middleware/JwtAuthentication');
 
-
 const productController = require('../controller/product.controller');
 
 const route = Router();
