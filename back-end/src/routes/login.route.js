e71cdb556593346508fe6163b6106c48699c23e4
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-28 17:19:14 -0300

    fix: token authorization

--- back-end/src/routes/login.route.js
+++ back-end/src/routes/login.route.js
@@ -1,11 +1,10 @@
 const { Router } = require('express');
-const { tokenAuthorization } = require('../middleware/JwtAuthentication');
 
 const loginController = require('../controller/login.controller');
 const { verifyPassword, verifyEmail } = require('../middleware/user.validation');
 
 const route = Router();
 
-route.post('/', tokenAuthorization, verifyEmail, verifyPassword, loginController.login);
+route.post('/', verifyEmail, verifyPassword, loginController.login);
 
 module.exports = route;
--- back-end/src/routes/product.route.js
+++ back-end/src/routes/product.route.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const { tokenAuthorization } = require('../middleware/JwtAuthentication');
+
 
 const productController = require('../controller/product.controller');
 
 const route = Router();
 
-route.get('/', productController.findProducts);
+route.get('/', tokenAuthorization, productController.findProducts);
 
 module.exports = route;
