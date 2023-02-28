03be7525f58acc244aa2ff33ad9abf36dcf93a50
Sheifer Kepler <sheifer.kda@gmail.com>
2023-02-28 12:53:46 -0300

    feat: findProduct route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routeUser = require('../routes/register.route');
+const routeProduct = require('../routes/product.route');
 
 const app = express();
 app.use(express.json());
 app.use('/register', routeUser);
+app.use('/customer/products', routeProduct);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
