637806057863e623642378111c873daa74534508
Victor Matias <fvictor1705@gmail.com>
2023-03-01 17:20:55 -0300

    refactor: restructure Register page

--- front-end/src/pages/Resgister.jsx
+++ front-end/src/pages/Resgister.jsx
@@ -1,5 +1,9 @@
+import axios from 'axios';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import GenericButton from '../components/GenericButton';
 import GenericInput from '../components/GenericInput';
+import { formContext } from '../context/FormProvider';
 import {
   validateEmailInput,
   validateNameInput,
@@ -7,9 +11,42 @@ import {
 } from '../utils/inputsVaidations';
 
 function Resgister() {
+  const navidateTo = useNavigate();
+  const { inputsValue: {
+    name, email, pass,
+  }, setInputsValue } = useContext(formContext);
+
   const onSubmit = (event) => {
     event.preventDefault();
   };
+
+  const onClick = async () => {
+    try {
+      const request = await axios.post('http://localhost:3001/register', {
+        name: name.value, email: email.value, password: pass.value,
+      });
+
+      console.log(request.data);
+
+      setInputsValue({
+        name: { value: '', isValid: false },
+        email: { value: '', isValid: false },
+        pass: { value: '', isValid: false },
+      });
+
+      navidateTo('/customer/products');
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  useEffect(() => {
+    setInputsValue({
+      name: { value: '', isValid: false },
+      email: { value: '', isValid: false },
+      pass: { value: '', isValid: false },
+    });
+  }, []);
 
   return (
     <form onSubmit={ onSubmit }>
@@ -18,25 +55,27 @@ function Resgister() {
         keyAccess="name"
         type="text"
         validation={ validateNameInput }
-        data-testid="common_register__input-name"
+        dataTestId="common_register__input-name"
       />
       <GenericInput
         name="Email"
         keyAccess="email"
         type="email"
         validation={ validateEmailInput }
-        data-testid="common_register__input-email"
+        dataTestId="common_register__input-email"
       />
       <GenericInput
         name="Senha"
         keyAccess="pass"
         type="password"
         validation={ validatePassInput }
-        data-testid="common_register__input-password"
+        dataTestId="common_register__input-password"
       />
       <GenericButton
         title="Cadastrar"
-        data-testid="common_register__button-register"
+        isDisabled={ !(name.isValid && email.isValid && pass.isValid) }
+        onClick={ onClick }
+        dataTestId="common_register__button-register"
       />
     </form>
   );
