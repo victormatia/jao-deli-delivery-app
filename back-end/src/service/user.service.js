24bdc118e681de5cee854ee2f4b9bc391cf0a2f1
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-02 16:41:46 -0300

    feat: find all users service

---
+++ back-end/src/service/user.service.js
@@ -0,0 +1,11 @@
+const { User } = require('../database/models');
+
+const findUsers = async () => {
+  const users = await User.findAll();
+
+  return {status: 200, result: users}
+};
+
+module.exports = {
+  findUsers
+};
