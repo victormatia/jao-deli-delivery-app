84434da2a10cba2a3cebc395174fd62a8d208c0a
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-01 21:30:45 -0300

    feat: register service test

---
+++ back-end/src/tests/register/register.service.test.js
@@ -0,0 +1,22 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const { User } = require('../../database/models');
+const { insertRegisterBody, userMock } = require('./mocks')
+const { createUser } = require('../../service/register.service');
+
+describe('testa a camada services para a rota /register', () => {
+
+  afterEach(sinon.restore);
+
+  describe('testa a função createUser', () => {
+    it('cria um novo usuário', async () => {
+
+      sinon.stub(User, 'create').resolves(userMock);
+
+      const response = await createUser(insertRegisterBody);
+
+      expect(response).to.deep.equal(userMock);
+    });
+  });
+});
