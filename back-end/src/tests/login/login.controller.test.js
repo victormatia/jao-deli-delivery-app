4861c141ac9c72dd25344f3d975f1eb759392ebf
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-03 16:35:36 -0300

    feat: teste de login informando um erro (controller)

--- back-end/src/tests/login/login.controller.test.js
+++ back-end/src/tests/login/login.controller.test.js
@@ -8,14 +8,33 @@ chai.use(sinonChai);
 const loginService = require('../../service/login.service')
 const loginController = require('../../controller/login.controller')
 
-const { insertRegisterBody, userAlreadyExists, insertUserBody, userServiceMock } = require('./mocks');
-const { generateToken } = require('../../utils/generate.token');
+const { userServiceMock } = require('./mocks');
 
 describe('testa a camada controllers para a rota /login', () => {
 
   afterEach(sinon.restore);
 
   describe('testa a função login', () => {
+    it('retorna um erro caso o usuário não seja encontrado', async () => {
+      const req = {
+        body: {
+          email: 'batatinha01@mail.com',
+          password: '123456',
+        }
+      };
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(loginService, 'login').resolves({ status: 401, message: 'Invalid password' });
+
+      await loginController.login(req, res);
+
+      expect(res.status).to.have.be.calledWith(401);
+      expect(res.json).to.have.be.calledWith({ status: 401, message: 'Invalid password' });
+    });
+
     it('encontra o usuário cadastrado', async () => {
       const req = {
         body: {
