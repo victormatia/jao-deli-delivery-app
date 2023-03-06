9296a64644cf8bf982929a330bc4e0f6ed1b6287
Sheifer Kepler <sheifer.kda@gmail.com>
2023-03-06 19:09:28 -0300

    feat: admin deleteUser service test

--- back-end/src/tests/admin/admin.service.test.js
+++ back-end/src/tests/admin/admin.service.test.js
@@ -2,7 +2,7 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 
 const { User } = require('../../database/models');
-const { findUsers } = require('../../service/admin.service');
+const { findUsers, deleteUser } = require('../../service/admin.service');
 const { usersMock } = require('./mock');
 
 describe('testa a camada services para a rota /admin', () => {
@@ -17,6 +17,28 @@ describe('testa a camada services para a rota /admin', () => {
       const response = await findUsers();
 
       expect(response).to.deep.equal({ status: 200, result: usersMock });
+    });
+  });
+
+  afterEach(sinon.restore);
+
+  describe('testa a função deleteUser', () => {
+    it('deleta um usuário pelo id', async () => {
+
+      sinon.stub(User, 'destroy').resolves(usersMock[2]);
+
+      const response = await deleteUser(3)
+
+      expect(response).to.deep.equal({ status: 204, result: usersMock[2] });
+    });
+
+    it('caso o usuário não exista retorna um erro', async () => {
+
+      sinon.stub(User, 'findByPk').resolves({ status: 404, message: 'User not found' });
+
+      const response = await deleteUser(4)
+
+      expect(response).to.deep.equal({ status: 404, message: 'User not found' });
     });
   });
 });
