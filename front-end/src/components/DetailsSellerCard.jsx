9b959dcbcb5107d91a12c50c43272faea5df6d08
Victor Matias <fvictor1705@gmail.com>
2023-03-19 15:53:39 -0300

    feat: add updateOrder function

--- front-end/src/components/DetailsSellerCard.jsx
+++ front-end/src/components/DetailsSellerCard.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import axios from 'axios';
+import GenericButton from './GenericButton';
+import getLocalStorage from '../utils/getLocalStorage';
 
-function DetailsSellerCard({ sale }) {
-  // const navigateTo = useNavigate();
+function DetailsSellerCard({ sale, updateSale }) {
   const dataTestId1 = 'seller_order_details__element-order-details-label-order-id';
   const dataTestId2 = 'seller_order_details__element-order-details-label-order-date';
   const dataTestId3 = 'seller_order_details__element-order-details-label-delivery-status';
+
+  const updateOrder = async (status) => {
+    try {
+      await axios.patch(`http://localhost:3001/customer/orders/${sale.id}`, {
+        status,
+      });
+
+      const { token } = await getLocalStorage('user');
+
+      const { data } = await axios.get(`http://localhost:3001/seller/orders/${sale.id}`, { headers: {
+        Authorization: token,
+      },
+      });
+
+      updateSale(data.result);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <section>
       <p data-testid={ dataTestId1 }>
-
         { sale.id }
       </p>
       <p
@@ -24,22 +44,22 @@ function DetailsSellerCard({ sale }) {
       >
         { sale.status }
       </h4>
-      <button
-        type="button"
-        data-testid="seller_order_details__button-preparing-check"
-        onClick={ () => {} }
+      <GenericButton
+        className=""
+        title="PREPARAR PEDIDO"
         isDisabled={ sale.status !== 'Pendente' }
-      >
-        PREPARAR PEDIDO
-      </button>
-      <button
-        type="button"
-        data-testid="seller_order_details__button-dispatch-check"
-        onClick={ () => {} }
-        disabled
-      >
-        SAIU PARA ENTREGA
-      </button>
+        onClick={ () => updateOrder('Preparando') }
+        icon=""
+        dataTestId="seller_order_details__button-preparing-check"
+      />
+      <GenericButton
+        className=""
+        title="SAIU PARA ENTREGA"
+        isDisabled={ sale.status !== ('Preparando' || 'Em Trânsito') }
+        onClick={ () => updateOrder('Em Trânsito') }
+        icon=""
+        dataTestId="seller_order_details__button-dispatch-check"
+      />
     </section>
   );
 }
